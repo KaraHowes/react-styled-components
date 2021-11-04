d56993e7075257c82a5965d76586695ca0503bb6
Kara Howes <karahowes@Karas-MacBook-Air.local>
2021-11-04 22:19:20 +0100

    set up and styled background, title and circle as styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,70 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
+const Header = styled.header `
+width: 100%;
+ height: 550px;
+ background-color: #151414
+`
+const navigation = styled.nav `
+display: flex;
+justify-content: flex-end;
+padding-right: 50px;
+padding-top: 50px;
+color: white;
+` 
+const anchor = styled.a `
+font-size: 30px;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 700;
+  height: 35px;
+  width: 110px;
+  margin-right: 20px;
+  color: white;
+`
+const Title = styled.h1 `
+font-family: 'Times New Roman', Times, serif;
+  font-size: 100px;
+  line-height: 122.2px;
+  position: absolute;
+  z-index: 1;
+  left: 250px;
+  top: 200px;
+  width: 684px;
+  height: 243px;
+  font-style: normal;
+  font-weight: bold;
+  text-shadow: 0px 1.5px 4px #FFF5D1;
+  color: white;
+`
+
+const Circle = styled.div`
+position: absolute;
+  width: 500px;
+  height: 500px;
+  border-radius: 50%;
+  top: -50px;
+  left: 106px;
+  background-color: #DD8723
+`
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Header>
+        <navigation>
+            <anchor>about</anchor>
+            <anchor>trips</anchor>
+            <anchor>pricing</anchor>
+            <anchor>contact</anchor>
+        </navigation>
+        <Title>Get ready for adventure!</Title>
+        
+       <div className="circle">
+           <Circle></Circle>
+            
+       </div>
+        
+    </Header>
   )
 }
